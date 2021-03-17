7ff623a8446795f449901897a7fcef6ddd928a93
Johan Lai Kine <jlaikine@pcalb-mm0000.univ-jfc.fr>
2021-03-17 11:45:57 +0100

    modif categorie

--- Projet_cuisine/src/Categorie.java
+++ Projet_cuisine/src/Categorie.java
@@ -1,10 +1,8 @@
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.util.Observable;
-import java.util.Observer;
 
-public class Categorie extends Canvas implements Observer {
+public class Categorie extends Canvas {
 
 	public static Toolkit t = Toolkit.getDefaultToolkit();
 	public static Dimension d = t.getScreenSize();
@@ -14,12 +12,6 @@ public class Categorie extends Canvas implements Observer {
 	
 	public Categorie() {
 		
-		
-	}
-	
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 
