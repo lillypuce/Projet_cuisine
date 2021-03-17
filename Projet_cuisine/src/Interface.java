422a1d24a0681606ae9c69a370218aa30acd93f3
Johan Lai Kine <jlaikine@pcalb-mm0000.univ-jfc.fr>
2021-03-17 10:57:54 +0100

    Modif class interface

--- Projet_cuisine/src/Interface.java
+++ Projet_cuisine/src/Interface.java
@@ -1,9 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class Interface extends Frame implements WindowListener{
-
+	
+	public static void main(String[] args) {
+		new Interface();
+	}
+	
+	public Interface() {
+		
+		this .setPreferredSize(new Dimension(640,340));
+		
+		this.setLayout(new BorderLayout());
+		
+		this.addWindowListener(this);
+		this.setTitle("Interface du projet");
+		this.pack();
+		this.setVisible(true);
+	}
+	
 	@Override
 	public void windowActivated(WindowEvent arg0) {
 		// TODO Auto-generated method stub
@@ -18,7 +36,7 @@ public class Interface extends Frame implements WindowListener{
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		System.exit(0);
 		
 	}
 
