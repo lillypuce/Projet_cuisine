32534825b5a450843269126fb6137bec47b76d34
Pauline Castelbou <pcastelb@pcalb-mm0000.univ-jfc.fr>
2021-03-24 12:14:24 +0100

    Changement Liste ingredient

--- Projet_cuisine/src/ListeIngredients.java
+++ Projet_cuisine/src/ListeIngredients.java
@@ -1,65 +1,58 @@
-
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject; 
-import org.json.simple.parser.*; 
+import org.json.simple.parser.*;
 
 
 public class ListeIngredients {
-	String id, type;
 	
-	public ListeIngredients() {
-		
+	String id, type;
+	String[] recettes;
+	
+	public ListeIngredients(String i,String t) {
+		this.id = i;
+		this.type = t;
+	}
+	
+	public ListeIngredients(String[] r) {
+		this.recettes = r;
 	}
 	
 	
-	
-	
-	 public static void main(String[] args)throws Exception  
-	    {  {
-		 //recup fichier ingredients.json
-		 Object obj = new JSONParser().parse(new FileReader("ingredient.json"));
-		// Object recobj = new JSONParser().parse(new FileReader("recette.json"));
-		 
-		 JSONObject jo = (JSONObject) obj; 
-		// JSONObject jr = (JSONObject) recobj; 
-		 
-		 //recup id et type
-		 String id = (String) jo.get("id");
-		 String type = (String) jo.get("type");
-		 
-		// String idr = (String) jr.get("id");
-		// String rec = (String) jr.get("recettes");
-		 //JSONArray ja = new JSONArray();
-		 //m = new LinkedHashMap(2); 
-	       // m.put(ja); 
-		 //ja.add(m);
-		 //jr.put(rec, ja); 
-		 
-		 System.out.println(id);
-		 System.out.println(type);
-		 
-		 //recup recettes(quand on les aura remplit avant)
-		// Map recettes =((Map)jo.get("ingredients"));
-		 
-		 JSONArray ja = (JSONArray) jo.get("ingredients");
+	public static void main(String[] args)throws Exception  {  
+		//Récupération fichier ingredients.json
+		
+		Object obj = new JSONParser().parse(new FileReader("ingredient.json"));
+		JSONObject jo = (JSONObject) obj; 
+		
+		//Récupération recettes(quand on les aura remplit avant)
+		JSONArray ja = (JSONArray) jo.get("ingredients");
+		
+		Map ingredient = new LinkedHashMap();
+		
 		for (int i=0;i<ja.size();i++) {
-			
 			JSONObject ja2 = (JSONObject)ja.get(i);
-			System.out.println(ja2.get("id"));
-			System.out.println(ja2.get("type"));
+			JSONArray ja3 = (JSONArray) ja2.get("recettes");
 			
-		}
-		 
- 
-	 
-	 }
-	    }
+			
+			
+			String id = (String) ja2.get("id");
+			String type = (String) ja2.get("type");
 
+
+			ListeIngredients l = new ListeIngredients(id,type);
+			ingredient.put(l,recette);
+		}
+		System.out.println(ingredient);
+	}
+	
+	public String toString() {
+		return this.id + " "+this.type;
+	}
 }
