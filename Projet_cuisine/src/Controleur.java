30de7ead0b7e52a199d76a7ad22d74c678207bb4
Leila Ahamziou Rechou <lahamzio@pcalb-mm0000.univ-jfc.fr>
2021-03-31 10:42:44 +0200

    crétion de la classe Controleur.java

---
+++ Projet_cuisine/src/Controleur.java
@@ -0,0 +1,19 @@
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class Controleur implements ItemListener {
+	
+	Modele modl;
+	
+	public Controleur(Modele m) {
+		this.modl=m;
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
