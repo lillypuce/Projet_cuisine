cc46905f40ec1a1b2b053e1bbd33788b18dc97be
Leila Ahamziou Rechou <lahamzio@pcalb-mm0000.univ-jfc.fr>
2021-03-31 10:27:35 +0200

    création de la classe ImagesModele

---
+++ Projet_cuisine/src/ImagesModele.java
@@ -0,0 +1,14 @@
+
+public class ImagesModele {
+	public String id;
+	public String chemin;
+	
+	
+	public ImagesModele() {}
+	
+	public ImagesModele(String id, String p) {
+		this.id = id;
+		this.chemin = p;
+	}
+
+}
