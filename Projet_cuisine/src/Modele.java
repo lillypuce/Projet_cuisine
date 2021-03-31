e1fbafb2c2e12389136243e3db147b7a7c01db06
Leila Ahamziou Rechou <lahamzio@pcalb-mm0000.univ-jfc.fr>
2021-03-31 10:28:50 +0200

    modification sur la classe Modele.java (accès au images)

--- Projet_cuisine/src/Modele.java
+++ Projet_cuisine/src/Modele.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Observable;
 
-public class Modele {
+
+
+public class Modele  extends Observable{
+	
+	public ArrayList<ImagesModele> images;
+	
+public Modele(String dir) {
+		
+		File repImages = new File(dir);
+		File[] imagesListe = repImages.listFiles();
+		this.images = new ArrayList<>();
+		for (File file : imagesListe) {
+			String nomImage = file.getName().split("\\.")[0];
+			this.images.add(new ImagesModele(nomImage, "images/"+file.getName()));
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
