8568684d33046266b040c24ac1f100757f8be400
jlaikine <laikinejohan@gmail.com>
2021-04-07 12:59:43 +0200

    Modif ListeRecettes

--- Projet_cuisine/src/ListeRecettes.java
+++ Projet_cuisine/src/ListeRecettes.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -14,38 +15,20 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class Recette extends Panel{
+public class ListeRecettes extends Panel{
 	
-	String id,nom, categorie, souscategorie;
-	ArrayList<String> ingredients;
-	ArrayList<String> consignes;
 	
-	public static String[] entrees = {"salade", "nem", "tartare"};
-	public static String[] plats = {"Margherita", "Oceane", "Magret"};
-	public static String[] desserts = {"yaourt", "flan", "ananas"};
-	public static String[] boissons = {"coca", "soda", "pepsi"};
+	Modele m;
 	
-	public Recette(ItemListener l) {
+	public ListeRecettes(ItemListener l, Modele m) {	
+		List plats = new List(29);
 		
-		List lst_recette = new List(29);
-		for (int i=0; i<plats.length; i++) {
-			lst_recette.add(Recette.plats[i]);
+		Iterator<RecetteModele> i = m.dicoRecettes.values().iterator();
+		while (i.hasNext()) {
+			plats.add(i.next().nom);
 		}
-		this.add(lst_recette);
-		lst_recette.addItemListener(l);
+		
+		this.add(plats);
+		plats.addItemListener(l);
 	}
-	
-	public Recette(String i, String n,String c, String sc) {
-		this.id=i;
-		this.nom=n;
-		this.categorie=c;
-		this.souscategorie=sc;
-		this.ingredients = new ArrayList<>();
-		this.consignes = new ArrayList<>();
-	}
-
-	public String toString() {
-		return this.id + " "+this.nom+" "+this.categorie+" "+this.souscategorie;
-	}
-	
 }
