20444a804acee0f2095758acd1acb24ae3c667f0
jlaikine <laikinejohan@gmail.com>
2021-04-11 23:10:38 +0200

    Modif Interface

--- Projet_cuisine/src/ListeRecettes.java
+++ Projet_cuisine/src/ListeRecettes.java
@@ -1,22 +1,9 @@
 import java.awt.List;
 import java.awt.Panel;
 import java.awt.event.ItemListener;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class ListeRecettes extends Panel{
-	
 	
 	Modele m;
 	
@@ -30,5 +17,6 @@ public class ListeRecettes extends Panel{
 		
 		this.add(plats);
 		plats.addItemListener(l);
+		
 	}
 }
