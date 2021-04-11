3de1406cab6b5ec56edf9087a110eaad18362cd9
jlaikine <laikinejohan@gmail.com>
2021-04-11 23:06:51 +0200

    Ajout classe Ajouter_recette

---
+++ Projet_cuisine/src/Ajouter_recette.java
@@ -0,0 +1,24 @@
+import java.awt.Button;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ajouter_recette extends Panel implements ActionListener{
+	
+	public Ajouter_recette() {
+		Button b = new Button("Ajouter recette");
+		b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+		});
+		this.add(b);
+	
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
