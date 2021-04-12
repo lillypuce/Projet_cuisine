63f340bae10ea2bcf5f3b55458f867db0144432f
lillypuce <leilaahamziourechou@gmail.com>
2021-04-12 17:36:20 +0200

    petit changement sur ImagesModèle

--- Projet_cuisine/src/ImagesModele.java
+++ Projet_cuisine/src/ImagesModele.java
@@ -2,13 +2,23 @@
 public class ImagesModele {
 	public String id;
 	public String chemin;
+	public int note;
 	
 	
 	public ImagesModele() {}
 	
-	public ImagesModele(String id, String p) {
+	public ImagesModele(String id, String p, int r) {
 		this.id = id;
 		this.chemin = p;
+		this.note=r;
+	}
+	
+	public String toString() {
+		String out=this.id+" ";
+		for (int i=0;i<=this.note;i++) {
+			out+="*";
+		}
+		return out;
 	}
 
 }
