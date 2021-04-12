b2be5866b9cc18a800b1b5485b48c45ef08c3255
jlaikine <laikinejohan@gmail.com>
2021-04-12 20:46:38 +0200

    lecture liste recette selon la categorie

--- Projet_cuisine/src/Categorie.java
+++ Projet_cuisine/src/Categorie.java
@@ -1,25 +1,31 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.List;
 import java.awt.Panel;
-import java.awt.Toolkit;
 import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Categorie extends Panel implements Observer{
 	
-	public static String[] categ = {"Entrees","Plats","Desserts", "Boissons"};
-	
-	List lst_categorie;
+	public static List lst_categorie;
+	int categorie_selectionnee=0;
 	
 	public Categorie(ItemListener l, Modele m) {
 		
 		lst_categorie = new List(29);
+		ArrayList<String> lst = new ArrayList<>();
 		
-		for (int i=0; i< categ.length; i++) {
-			lst_categorie.add(Categorie.categ[i]);
+		for (HashMap.Entry<String, RecetteModele> entry : m.dicoRecettes.entrySet()) {
+			String key = entry.getKey();
+			
+			
+			if (!lst.contains(m.dicoRecettes.get(key).categorie)) {	
+				lst_categorie.add(m.dicoRecettes.get(key).categorie);
+				lst.add(m.dicoRecettes.get(key).categorie);
+			}
 		}
+		
 		this.add(lst_categorie);
 		lst_categorie.addItemListener(l);
 		lst_categorie.select(0);
