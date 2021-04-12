62c35adea843e9d9afb9736ddb3638bc282dee83
jlaikine <laikinejohan@gmail.com>
2021-04-12 04:48:23 +0200

    Commencement controleur pour certaines classes

--- Projet_cuisine/src/Controleur.java
+++ Projet_cuisine/src/Controleur.java
@@ -1,5 +1,6 @@
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Arrays;
 
 public class Controleur implements ItemListener {
 	
@@ -12,7 +13,9 @@ public class Controleur implements ItemListener {
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource() instanceof java.awt.List) {
+			this.modl.select_categ((Integer)e.getItem());	
+		}
 		
 	}
 
