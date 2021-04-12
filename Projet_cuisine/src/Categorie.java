516e95834f5e4e9b86382cd2855e29aeeddcd36c
jlaikine <laikinejohan@gmail.com>
2021-04-12 04:47:27 +0200

    modif categorie pour changer recette (pas fini)

--- Projet_cuisine/src/Categorie.java
+++ Projet_cuisine/src/Categorie.java
@@ -4,20 +4,34 @@ import java.awt.List;
 import java.awt.Panel;
 import java.awt.Toolkit;
 import java.awt.event.ItemListener;
+import java.util.Observable;
+import java.util.Observer;
 
-public class Categorie extends Panel {
+public class Categorie extends Panel implements Observer{
 	
 	public static String[] categ = {"Entrees","Plats","Desserts", "Boissons"};
 	
-	public Categorie(ItemListener l) {
+	List lst_categorie;
+	
+	public Categorie(ItemListener l, Modele m) {
 		
-		List lst_categorie = new List(29);
+		lst_categorie = new List(29);
 		
 		for (int i=0; i< categ.length; i++) {
 			lst_categorie.add(Categorie.categ[i]);
 		}
 		this.add(lst_categorie);
 		lst_categorie.addItemListener(l);
+		lst_categorie.select(0);
+		
+		m.addObserver(this);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		int i = this.lst_categorie.getSelectedIndex();
+		this.lst_categorie.replaceItem(arg.toString(), i);
+		this.lst_categorie.select(i);
 	}
 
 }
