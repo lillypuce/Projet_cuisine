2f2beb398b59eae413c4332c9ed2351cc4fcc578
jlaikine <laikinejohan@gmail.com>
2021-04-14 12:44:35 +0200

    Affichage des consignes

--- Projet_cuisine/src/Affichage_recette.java
+++ Projet_cuisine/src/Affichage_recette.java
@@ -2,6 +2,7 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.List;
 import java.awt.Panel;
+import java.awt.TextArea;
 import java.awt.event.ItemListener;
 import java.util.Observable;
 import java.util.Observer;
@@ -9,19 +10,25 @@ import java.util.Observer;
 
 public class Affichage_recette extends Panel implements Observer{
 	
-	public static List liste;
+	public TextArea text;
+	public String s="";
+	Modele m;
 	
 	public Affichage_recette(ItemListener l, Modele m) {
-		liste = new List(29);
+		for (int i=0; i<m.dicoRecettes.get("poisson_pane").consignes.size(); i++) {
+			s=s+m.dicoRecettes.get("poisson_pane").consignes.get(i);
+			s=s+"\n";
+		}
+		text = new TextArea(s,32,100);
 		
-		liste.add(m.dicoRecettes.get("poisson_pane").nom);
-		this.add(liste);
+		text.setEditable(false);
+		this.add(text);
+		m.addObserver(this);
 		
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
 	}
 }
