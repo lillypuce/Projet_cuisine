de3e8af12e16bfea38b7d8e084922b29489630d1
jlaikine <laikinejohan@gmail.com>
2021-04-16 18:10:18 +0200

    Petite modif categorie

--- Projet_cuisine/src/Categorie.java
+++ Projet_cuisine/src/Categorie.java
@@ -9,7 +9,6 @@ import java.util.Observer;
 public class Categorie extends Panel implements Observer{
 	
 	public static List lst_categorie;
-	int categorie_selectionnee=0;
 	
 	public Categorie(ItemListener l, Modele m) {
 		
@@ -19,25 +18,24 @@ public class Categorie extends Panel implements Observer{
 		for (HashMap.Entry<String, RecetteModele> entry : m.dicoRecettes.entrySet()) {
 			String key = entry.getKey();
 			
-			
 			if (!lst.contains(m.dicoRecettes.get(key).categorie)) {	
 				lst_categorie.add(m.dicoRecettes.get(key).categorie);
 				lst.add(m.dicoRecettes.get(key).categorie);
 			}
 		}
-		
 		this.add(lst_categorie);
 		lst_categorie.addItemListener(l);
 		lst_categorie.select(0);
 		
 		m.addObserver(this);
+	
 	}
+
+
 
 	@Override
 	public void update(Observable o, Object arg) {
-		int i = this.lst_categorie.getSelectedIndex();
-		this.lst_categorie.replaceItem(arg.toString(), i);
-		this.lst_categorie.select(i);
+		
 	}
 
 }
