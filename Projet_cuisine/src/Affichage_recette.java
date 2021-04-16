c1d78269c18c19f800c15e886ba9d97660746589
jlaikine <laikinejohan@gmail.com>
2021-04-17 01:21:32 +0200

    amelioration affichage recette

--- Projet_cuisine/src/Affichage_recette.java
+++ Projet_cuisine/src/Affichage_recette.java
@@ -1,3 +1,5 @@
+import java.awt.Font;
+import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextArea;
 import java.awt.event.ItemListener;
@@ -9,29 +11,40 @@ public class Affichage_recette extends Panel implements Observer{
 	
 	public TextArea text;
 	public String s="";
+	
 	Modele m;
 	
 	public Affichage_recette(ItemListener l, Modele m) {
+		
+		Label consignes = new Label("CONSIGNES");
+		consignes.setFont(new Font("Serif", Font.BOLD, 15));
+		
+		Label ingredients = new Label("INGREDIENTS");
+		ingredients.setFont(new Font("Serif", Font.BOLD, 15));
+		
 		for (int i=0; i<m.dicoRecettes.get("poisson_pane").consignes.size(); i++) {
 			s=s+m.dicoRecettes.get("poisson_pane").consignes.get(i);
 			s=s+"\n";
+			
 		}
-		text = new TextArea(s,28,65);
+		text = new TextArea(s,10,65);
 		
 		text.setEditable(false);
+		this.add(consignes);
 		this.add(text);
+		this.add(ingredients);
 		m.addObserver(this);
-		
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		text.setText("");
-		s = "";
-		s = m.aff_recette;
 		
+		text.setText("");
+		s = m.aff_recette;
 		text.setText(s);
 		text.setEditable(false);
+		
 		this.add(text);
+		
 	}
 }
