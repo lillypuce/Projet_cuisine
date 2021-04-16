dfe348d7c8c013c53d851b10b1c121bcef6eee0d
jlaikine <laikinejohan@gmail.com>
2021-04-16 18:19:43 +0200

    Modif taille de la barre de recherche

--- Projet_cuisine/src/Recherche_recette.java
+++ Projet_cuisine/src/Recherche_recette.java
@@ -1,13 +1,12 @@
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Panel;
-import java.awt.TextArea;
 import java.awt.TextField;
 
 public class Recherche_recette extends Panel {
 	
 	public Recherche_recette(){
-		 TextField t = new TextField("Tapez ici pour rechercher une recette � partir d'ingr�dients"); 
+		 TextField t = new TextField("Tapez ici pour rechercher une recette"); 
 		 Button b = new Button("Rechercher");
 		 
 		 b.setForeground(Color.WHITE);
