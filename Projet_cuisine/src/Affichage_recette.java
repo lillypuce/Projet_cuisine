55c2049dd57b994c6ea9060c2207809f0e5c8563
jlaikine <laikinejohan@gmail.com>
2021-04-17 23:03:47 +0200

    affichage des noms des recettes au centre

--- Projet_cuisine/src/Affichage_recette.java
+++ Projet_cuisine/src/Affichage_recette.java
@@ -10,17 +10,16 @@ import java.util.Observer;
 public class Affichage_recette extends Panel implements Observer{
 	
 	public TextArea text;
-	public String s="";
+	public String s="", s2;
+	public static Label titre;
 	
 	Modele m;
 	
 	public Affichage_recette(ItemListener l, Modele m) {
 		
-		Label consignes = new Label("CONSIGNES");
-		consignes.setFont(new Font("Serif", Font.BOLD, 15));
-		
-		Label ingredients = new Label("INGREDIENTS");
-		ingredients.setFont(new Font("Serif", Font.BOLD, 15));
+		s2 = "POISSON PANE";
+		titre = new Label(s2);
+		titre.setFont(new Font("Serif", Font.BOLD, 18));
 		
 		for (int i=0; i<m.dicoRecettes.get("poisson_pane").consignes.size(); i++) {
 			s=s+m.dicoRecettes.get("poisson_pane").consignes.get(i);
@@ -30,21 +29,23 @@ public class Affichage_recette extends Panel implements Observer{
 		text = new TextArea(s,10,65);
 		
 		text.setEditable(false);
-		this.add(consignes);
+		this.add(titre);
 		this.add(text);
-		this.add(ingredients);
 		m.addObserver(this);
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		
 		text.setText("");
 		s = m.aff_recette;
 		text.setText(s);
 		text.setEditable(false);
 		
-		this.add(text);
+		s2 = m.nom_recette_selectionnee.toUpperCase();
+		titre.setText(s2);
+		titre.setBounds(330-((s2.length()/2)*12),12,500,13);
 		
+		this.add(titre);
+		this.add(text);
 	}
 }
