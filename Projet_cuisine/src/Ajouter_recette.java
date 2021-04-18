f4d7c620e49397c7e2fe864950f6b3df24b3c336
lillypuce <leilaahamziourechou@gmail.com>
2021-04-18 17:00:15 +0200

    on relie les 2 interfaces avec le bouton <<Ajouter recette>>

--- Projet_cuisine/src/Ajouter_recette.java
+++ Projet_cuisine/src/Ajouter_recette.java
@@ -2,6 +2,9 @@ import java.awt.Button;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
 
 public class Ajouter_recette extends Panel implements ActionListener{
 
@@ -10,6 +13,21 @@ public class Ajouter_recette extends Panel implements ActionListener{
 		b.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+            	Interface I;
+            	Interface_fen_2 I2;
+				try {
+					I2 = new Interface_fen_2();
+					I2.setVisible(true);
+					
+					//I = new Interface();
+					//I.setVisible(false);
+				} catch (IOException | ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            	
+            	
+            	
 
             }
 		});
