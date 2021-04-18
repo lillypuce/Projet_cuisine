abc28fe1a683923306b7e888f32ce20f85673a06
lillypuce <leilaahamziourechou@gmail.com>
2021-04-18 11:32:18 +0200

    Modification de l'apparance de la 2ème fenêtre

--- Projet_cuisine/src/Ajouter_recette.java
+++ Projet_cuisine/src/Ajouter_recette.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,54 +14,77 @@ public class Ajouter_recette extends Panel implements ActionListener{
 	
 	public Ajouter_recette() {
 		
-		Frame f= new Frame("Test");
+		//Frame f= new Frame("Test");
 		
-		Label l1,l2,l3,l4;
+		Label l0,l1,l2,l3,l4,l5,l6;
+		TextArea text;
+		String s="ex: 1. Préchauffez le four Th8 (240°C). ";
 		
-		l1=new Label("Tapez ici la catÃ©gorie de la recette");
-		l1.setBounds(50,50,250,20);
+		l0=new Label("Nouvelle recette");
+		l0.setBounds(50,0,250,20);
+		
+		l1=new Label("Tapez ici la catégorie de la recette");
+		l1.setBounds(50,80,250,20);
 		TextField cat = new TextField("ex: Plats"); 
 		cat.setBounds(50,100,250,20);
 		
-		l2=new Label("Tapez ici le nom de la recette");
-		l2.setBounds(50,150,250,20);
-		TextField tnom = new TextField("ex: Tarte aux pommes..."); 
-		tnom.setBounds(50,200,250,20);
+		l2=new Label("Tapez ici la sous-catégorie de la recette");
+		l2.setBounds(50,180,250,20);
+		TextField scat = new TextField("ex: Pizza"); 
+		scat.setBounds(50,200,250,20);
 		
-		l3=new Label("Tapez ici la liste des ingrÃ©dients");
-		l3.setBounds(50,250,250,20);
+		l3=new Label("Tapez ici le nom de la recette");
+		l3.setBounds(50,280,250,20);
+		TextField tnom = new TextField("ex: Margherita..."); 
+		tnom.setBounds(50,300,250,20);
+		
+		l4=new Label("Tapez ici la liste des ingrédients");
+		l4.setBounds(50,380,250,20);
 		TextField ing = new TextField("ex: sel,poivre..."); 
-		ing.setBounds(50,300,250,20);
+		ing.setBounds(50,400,250,20);
 		
-		l4=new Label("Tapez ici la quatitÃ© de l'ingrÃ©dients");
-		l4.setBounds(50,400,250,20);
-		TextField dos = new TextField("ex: 2"); 
-		dos.setBounds(50,450,250,20);
+		l5=new Label("Tapez ici la quatité de l'ingrédients");
+		l5.setBounds(50,480,250,20);
+		TextField dos = new TextField("ex: 2g"); 
+		dos.setBounds(50,500,250,20);
+		
+		l6=new Label("Entrez ici les consignes de la recette");
+		l6.setBounds(450,80,250,20);
+		text = new TextArea(s,100,65);
+		text.setEditable(true);
+		text.setBounds(450, 100, 400, 400);
+		
 		
 		Button b = new Button("Ajouter recette");
 		 
 		 b.setForeground(Color.WHITE);
 		 b.setBackground(new Color(200,200,200));
+		 b.setBounds(50,550,250,20);
 		 
+		 this.add(l0);
 		 
-		 f.add(l1);
-		 f.add(cat);
+		 this.add(l1);
+		 this.add(cat);
 		 
-		 f.add(l2);
-		 f.add(tnom);
+		 this.add(l2);
+		 this.add(scat);
+		 
+		 this.add(l3);
+		 this.add(tnom);
 		 		 
-		 f.add(l3);
-		 f.add(ing);
+		 this.add(l4);
+		 this.add(ing);
 		 
-		 f.add(l4);
-		 f.add(dos);
+		 this.add(l5);
+		 this.add(dos);
 		 
+		 this.add(l6);
+		 this.add(text);
 		 
-		 
-		 f.add(b);
-		 f.setSize(900,900);
-		 f.setLayout(null);
-		 f.setVisible(true);
+		 this.add(b);
+		 this.setSize(900,900);
+		 this.setLayout(null);
+		 this.setVisible(true);
 		
 		
 		b.addActionListener(new ActionListener() {
