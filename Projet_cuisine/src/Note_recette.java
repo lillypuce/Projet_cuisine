bdfce9389a455b6b8bc968b7e9eead94d0b1d57e
jlaikine <laikinejohan@gmail.com>
2021-04-23 01:48:52 +0200

    ajout boutons pour la fonctionnalité liste des courses

--- Projet_cuisine/src/Note_recette.java
+++ Projet_cuisine/src/Note_recette.java
@@ -10,11 +10,13 @@ public class Note_recette extends Panel{
 	
 	public static String[] libelles = {"1 etoile", "2 etoiles", "3 etoiles", "4 etoiles"};
 	
-	public Note_recette(ItemListener l, Ajouter_recette ar, Recherche_recette rr) {
+	public Note_recette(ItemListener l, Ajouter_recette ar, Recherche_recette rr, AjouterCourses ac, ListeCourses lc) {
 		
 		this.setLayout(new FlowLayout());
 		this.add(rr);
 		this.add(ar);
+		this.add(ac);
+		this.add(lc);
 		
 		CheckboxGroup cg=new CheckboxGroup();
 		for (int i=0; i<4; i++) {
