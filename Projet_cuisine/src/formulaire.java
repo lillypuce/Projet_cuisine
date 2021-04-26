571290a68facb8ff118a3907ecf978bf1bdfee5f
jlaikine <laikinejohan@gmail.com>
2021-04-26 17:05:29 +0200

    fonctionnalité ajout recette

--- Projet_cuisine/src/formulaire.java
+++ Projet_cuisine/src/formulaire.java
@@ -1,4 +1,5 @@
 import java.awt.Button;
+import java.awt.Choice;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Label;
@@ -7,17 +8,25 @@ import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import javax.swing.JOptionPane;
+
+import org.json.simple.parser.ParseException;
 
 public class formulaire extends Panel implements ActionListener{
 	
 	Modele m;
-	public formulaire() {
+	
+	public formulaire() throws FileNotFoundException, IOException, ParseException {
 		
 		Label l0,l1,l2,l3,l4,l5,l6;
 		TextArea text;
@@ -27,15 +36,20 @@ public class formulaire extends Panel implements ActionListener{
 		l0.setBounds(50,10,250,40);
 		
 		
-		l1=new Label("Tapez ici la catégorie de la recette");
+		l1=new Label("Sélectionnez ici la catégorie de la recette");
 		//l1.setFont(new Font("Arial", Font.BOLD, 12));
 		l1.setBounds(50,80,250,20);
-		TextField cat = new TextField("ex: Plats"); 
-		cat.setBounds(50,100,250,20);
+		Choice cat = new Choice();
+		cat.addItem("Entrée");
+		cat.addItem("Plats");
+		cat.addItem("Dessert");
+		
+		cat.setBounds(50,100,380,20);
 		
 		l2=new Label("Tapez ici la sous-catégorie de la recette");
 		l2.setBounds(50,180,250,20);
 		TextField scat = new TextField("ex: Pizza"); 
+		scat.selectAll();
 		scat.setBounds(50,200,250,20);
 		
 		l3=new Label("Tapez ici le nom de la recette");
@@ -50,7 +64,7 @@ public class formulaire extends Panel implements ActionListener{
 		
 		l5=new Label("Tapez ici la quatité de l'ingrédients");
 		l5.setBounds(50,480,250,20);
-		TextField dos = new TextField("ex: 2g"); 
+		TextField dos = new TextField("ex: 2g,3g"); 
 		dos.setBounds(50,500,250,20);
 		
 		l6=new Label("Entrez ici les consignes de la recette");
@@ -71,51 +85,89 @@ public class formulaire extends Panel implements ActionListener{
 		 b.addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
-	            	String tcat=cat.getText();
+	            	
+	            	String tcat=cat.getSelectedItem();
 	            	String tscat=scat.getText();
 	            	String texnom=tnom.getText();
 	            	String ting=ing.getText();
 	            	String tdos=dos.getText();
-	            	String Ttext=text.getText();
+	            	String tconsig=text.getText();
 	                
 	            	String tid=texnom.toLowerCase ();
 	            	tid = tid.replaceAll("\\s", "_");
 	            	
-	            	String[] Tting =ting.split(",",5);
+	            	//Ingrédients et dosages
+	            	String[] noming = ting.split(",");
+	            	String[] ingdos = tdos.split(",");
 	            	
-	            	String[] Ttdos = tdos.split(",",5);
-	            
-	            	for (int i=0;i<Tting.length;i++) {
-	            		String t=Tting[i];
-	            		String ti=t.toLowerCase ();
-	            		ti = ti.replaceAll("\\s", "_");
-	            		
-	            		///on met les nouvelles donnée dans dicoIngredients
-	            		Ingredient b = new Ingredient(ti,t);
-	            		m.dicoIngredients.put(ti,b);
-	            		Ingredient ingredient_test2 = m.dicoIngredients.get(ti);
-	            		if(ingredient_test2.id.compareTo(ti) == 0) {
-	            			if(ingredient_test2.recettes.contains(tid) == false) {
-	            				ingredient_test2.recettes.add(tid);
-	            				
-	            			}
-	            			
-	            		}
+	            	ArrayList<ArrayList<String>> al = new ArrayList<>();
+	            	ArrayList<String> al2 = new ArrayList<>();
+	            	
+	            	if (noming.length != ingdos.length) {
+	            		JOptionPane.showMessageDialog(null, "Les valeurs entrées dans les ingrédients et le dosage sont incorrectes");
 	            	}
-	            	
-	            	
-	            	///on met les nouvelles donnée dans dicoRecettes
-	            	RecetteModele r= new RecetteModele(tid,texnom,tcat,tscat);
-	            	m.dicoRecettes.put(tid, r);
-	            	
-	                
-	                System.out.println(m.dicoIngredients);
-	                System.out.println(m.dicoRecettes);
-	                 
+	            	else {
+		            	for (int i=0;i<noming.length;i++) {
+		            		String noming1=noming[i];
+		            		String ti=noming1.toLowerCase ();
+		            		ti = ti.replaceAll("\\s", "_");
+		            		
+		            		String ingdos1 = ingdos[i];
+		            		String td = ingdos1.toLowerCase ();
+		            		
+		            		al2.add(ti);
+		            		al2.add(td);
+		            		al.add(al2);
+		            		al2 = new ArrayList<>();
+		            	}
+		            	
+		            	//Consignes
+		            	String[] consig = tconsig.split("\n");
+		            	
+		            	ArrayList<String> al3 = new ArrayList<>();
+		            	
+		            	for (int i=0; i<consig.length; i++) {
+		            		al3.add(consig[i]);
+		            	}
+		            	
+		            	RecetteModele r= new RecetteModele(tid,texnom,tcat,tscat);
+		                
+		            	r.affichage_ingredients = al;
+		            	r.consignes = al3;
+		            	
+		            	m.dicoRecettes.put(tid, r);
+		            	
+		            	//Insertion des recettes dans les listes des categories pour ensuite être lu par les autres classes
+		            	if (m.dicoRecettes.get(tid).categorie.equals("Plats")) {
+		            		ListeRecettes.lst_plats.add(m.dicoRecettes.get(tid).nom);
+		            	}
+		            	if (m.dicoRecettes.get(tid).categorie.equals("Dessert")) {
+		            		ListeRecettes.lst_desserts.add(m.dicoRecettes.get(tid).nom);
+		            	}
+		            	if (m.dicoRecettes.get(tid).categorie.equals("Entrée")) {
+		            		ListeRecettes.lst_entree.add(m.dicoRecettes.get(tid).nom);
+		            	}
+
+		            	try {
+							FileWriter fw = new FileWriter("recetteajoutee.txt", true);
+							fw.write("[\n");
+							fw.write(tid + "\n");
+							fw.write(texnom + "\n");
+							fw.write(tcat + "\n");
+							fw.write(tscat + "\n");
+							fw.write(ting + "\n");
+							fw.write(tdos + "\n");
+							fw.write(tconsig + "\n");
+							fw.write("]\n");
+							fw.close();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		            	
+	            	}
 	            }
 			});
-		 
-		 
 		 
 		 this.add(l0);
 		 
